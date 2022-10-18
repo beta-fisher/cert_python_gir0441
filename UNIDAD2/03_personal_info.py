6b7d2c2e287442e246dbbec94a60dbbf756edf0d
beta <ravacbt@gmail.com>
2022-10-18 09:19:10 -0500

    ejercicios

--- UNIDAD2/03_personal_info.py
+++ UNIDAD2/03_personal_info.py
@@ -4,6 +4,8 @@
  18 de octubre 2022
 '''
 
+#El space en el documento decia variable pero la puse como constante
+space = " "
 #Asignaciónd de valores por parte del usuario por proceso de pregunta
 name = input("¿Cuál es su nombre? ")
 lastname = input("¿Cuál es su apellido? ")
@@ -12,3 +14,4 @@ age = input("¿Qué edad tiene usted? ")
 
 #Impresion con concatenación
 print("Hola!", name, lastname, "de", location, "y tiene", age, "años")
+print("Hola!",space,name,space,lastname,space,"de",space,location,space,"y tiene",space,age,space,"años")

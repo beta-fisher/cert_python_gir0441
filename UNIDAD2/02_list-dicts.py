3ab5320eeee723975354b65b6162c81df96bf3f6
beta <ravacbt@gmail.com>
2022-10-18 09:15:28 -0500

    ejercicios

--- UNIDAD2/02_list-dicts.py
+++ UNIDAD2/02_list-dicts.py
@@ -5,18 +5,31 @@ country = [ "Brazil", "Russia", "India", "China", "South Africa"]
 Note that South Africa has 3 capitals. Therefore, we embed a list inside
 the dictionary.
 """
-capitals = [ "Brazil: Brasilia","Russia: Moscow","India: New Delhi","China: Beijing", "South Africa: Pretoria, Cape Town,Bloemfontein"]
+capitals = { "Brazil": "Brasilia",
+            "Russia": "Moscow", 
+            "India": "New Delhi",
+            "China": "Beijing", 
+            "South Africa": 
+                            ["Pretoria", 
+                             "Cape Town",
+                             "Bloemfontein"]
+           }
 
 # Print the list and dictionary
 print( "country" )
+for c in country:
+    print (c)
 print( "capitals" )
+for cap in capitals:
+    print(cap)
+    print(capitals[cap])
 """
 What response did you get?
 Why did the list and dictionary contents not print?
 Fix the code and run the script again.
 """
 
-print( capitals[4])
+print(capitals ["South Africa"][1])
 """
 Why did you get an error for the 2nd capital of South Africa?
 Hint: Check the syntax for the index value.
---
+++ UNIDAD2/03_personal_info.py
@@ -0,0 +1,14 @@
+'''
+ Author: Angel Armando Ramírez Vázquez
+ Personal info: Programa pregunta piezas de informació y las imprime
+ 18 de octubre 2022
+'''
+
+#Asignaciónd de valores por parte del usuario por proceso de pregunta
+name = input("¿Cuál es su nombre? ")
+lastname = input("¿Cuál es su apellido? ")
+location = input("¿Donde vive? ")
+age = input("¿Qué edad tiene usted? ")
+
+#Impresion con concatenación
+print("Hola!", name, lastname, "de", location, "y tiene", age, "años")

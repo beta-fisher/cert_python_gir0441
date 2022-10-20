c29cead37dc97f864a96863f6ce789838439453a
Armando <106275334+beta-fisher@users.noreply.github.com>
2022-10-20 09:52:29 -0500

    Update 07_file-access_actvity.py

--- UNIDAD2/07_file-access_actvity.py
+++ UNIDAD2/07_file-access_actvity.py
@@ -1,6 +1,6 @@
 file = open("devices.txt", "a")
 while True:
-    newItem = input("Dispositivo para agrefar: ")
+    newItem = input("Dispositivo para agregar: ")
     if newItem == "exit":
         print("All done!")
         break
